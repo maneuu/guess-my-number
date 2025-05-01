01c58509b4e2c12819a3de201687618219520aca
Emanoel <eoemanoel@gmail.com>
2025-05-01 15:01:17 -0300

    implement number guessing game logic

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //   console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+  }
+});
